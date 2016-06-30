cdd499943d1a7b568aaee38e4c284f72f0fbc019
Vitaliy Prozapas <danteakuma7@gmail.com>
2016-06-30 20:25:41 +0300

    Create project via Android Studio

---
+++ app/src/main/java/com/allteran/realevidence/activities/MainActivity.java
@@ -0,0 +1,10 @@
+package com.allteran.realevidence.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Allteran on 30.06.2016.
+ */
+public class MainActivity extends AppCompatActivity {
+
+}
